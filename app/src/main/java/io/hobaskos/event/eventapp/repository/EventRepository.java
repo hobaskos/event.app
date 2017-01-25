227ac45bb12d055e29e3a3a38d26736d387930d7
Alexander Skjolden <alex@plastboks.net>
2017-01-25 16:53:34 +0100

    basic test (wiremock) for the eventapi
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- app/src/main/java/io/hobaskos/event/eventapp/repository/EventRepository.java
+++ app/src/main/java/io/hobaskos/event/eventapp/repository/EventRepository.java
@@ -1,18 +1,20 @@
 package io.hobaskos.event.eventapp.repository;
 
+import java.io.IOException;
 import java.util.List;
 
 import io.hobaskos.event.eventapp.api.EventAPI;
 import io.hobaskos.event.eventapp.api.EventService;
 import io.hobaskos.event.eventapp.models.Event;
+import retrofit2.Response;
 
 /**
  * Created by andre on 1/25/2017.
  */
 public class EventRepository {
 
-    public List<Event> getEvents() {
+    public Response<List<Event>> getEvents() throws IOException {
         EventAPI api = EventService.createService();
-        return api.getEvents();
+        return api.getEvents().execute();
     }
 }
