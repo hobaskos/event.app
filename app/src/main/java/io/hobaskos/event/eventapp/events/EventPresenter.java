5f6d6bc4ecc85032629a834af39bd8b53c3abed0
Alexander Skjolden <alex@plastboks.net>
2017-01-26 14:33:26 +0100

    injection fix
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- app/src/main/java/io/hobaskos/event/eventapp/events/EventPresenter.java
+++ app/src/main/java/io/hobaskos/event/eventapp/events/EventPresenter.java
@@ -1,5 +1,8 @@
 package io.hobaskos.event.eventapp.events;
 
+import javax.inject.Inject;
+
+import io.hobaskos.event.eventapp.App;
 import io.hobaskos.event.eventapp.repository.EventRepository;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
@@ -8,12 +11,17 @@ import rx.subscriptions.CompositeSubscription;
  * Created by andre on 1/26/2017.
  */
 public class EventPresenter {
-    private final EventRepository repository;
+
+    @Inject
+    public EventRepository repository;
+
     private final EventView view;
     private CompositeSubscription subscriptions;
 
-    public EventPresenter(EventRepository repository, EventView view) {
-        this.repository = repository;
+    public EventPresenter(EventView view) {
+
+        App.getInst().getDiComponent().inject(this);
+
         this.view = view;
         this.subscriptions = new CompositeSubscription();
     }
