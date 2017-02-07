786cc4bbced8e869fb53e7fff41d58f56dc0ecb5
zlokex <andre.hovda@gmail.com>
2017-02-07 13:30:24 +0100

    #FRONTEND-19 set eventsfragment as initial/default fragment in MainActivity

--- app/src/main/java/io/hobaskos/event/eventapp/ui/main/MainActivity.java
+++ app/src/main/java/io/hobaskos/event/eventapp/ui/main/MainActivity.java
@@ -38,6 +38,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Temp solution, Initial fragment:
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, new EventsFragment()).commit();
     }
 
     /**
