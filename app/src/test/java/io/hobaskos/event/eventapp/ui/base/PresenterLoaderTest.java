e33e7a34718b7b019d2a4ef8121fcc0ce1bc4e47
zlokex <andre.hovda@gmail.com>
2017-02-09 13:14:40 +0100

    adds SupportLoaderTestCase for testing loader supportv4

---
+++ app/src/test/java/io/hobaskos/event/eventapp/ui/base/PresenterLoaderTest.java
@@ -0,0 +1,19 @@
+package io.hobaskos.event.eventapp.ui.base;
+
+/**
+ * Created by andre on 2/9/2017.
+ */
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import io.hobaskos.event.eventapp.BuildConfig;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class PresenterLoaderTest {
+
+
+
+}
