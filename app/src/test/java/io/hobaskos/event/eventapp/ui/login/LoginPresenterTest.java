575249f0e909514135a00a9fe13a9ba1f76868a5
Hans Petter Osvold <hans@osvold.io>
2017-02-16 13:59:43 +0100

    #Frontend-11 : Rewrote JwtRepository Login-method to return a Observable that the LoginPresenter can subscribe to.

---
+++ app/src/test/java/io/hobaskos/event/eventapp/ui/login/LoginPresenterTest.java
@@ -0,0 +1,78 @@
+package io.hobaskos.event.eventapp.ui.login;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import io.hobaskos.event.eventapp.data.model.LoginVM;
+import io.hobaskos.event.eventapp.data.repository.JwtRepository;
+import rx.Observable;
+import rx.Scheduler;
+import rx.android.plugins.RxAndroidPlugins;
+import rx.android.plugins.RxAndroidSchedulersHook;
+import rx.schedulers.Schedulers;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by osvold.hans.petter on 15.02.2017.
+ */
+
+public class LoginPresenterTest {
+
+    @Mock
+    private JwtRepository repository;
+
+    private LoginPresenter loginPresenter;
+
+    @Before
+    public void setup()
+    {
+        MockitoAnnotations.initMocks(this);
+        repository = mock(JwtRepository.class);
+        loginPresenter = new LoginPresenter(repository);
+
+        RxAndroidPlugins.getInstance().registerSchedulersHook(new RxAndroidSchedulersHook() {
+            @Override
+            public Scheduler getMainThreadScheduler() {
+                return Schedulers.immediate();
+            }
+        });
+    }
+
+    @After
+    public void tearDown()
+    {
+        RxAndroidPlugins.getInstance().reset();
+    }
+
+    @Test
+    public void loginWithWrongCredentialsShouldCallOnErrorMethod()
+    {
+        LoginVM loginVM = new LoginVM("user", "admin", true);
+
+        repository.login(loginVM, loginPresenter);
+
+        when(repository.login(loginVM, loginPresenter)).thenReturn(Observable.create((subscriber) -> {
+            subscriber.onError(new Exception());
+            subscriber.onCompleted();
+        }));
+
+
+    }
+
+    @Test
+    public void loginWithCorrectCredentialsShouldCallSuccessMethod()
+    {
+        LoginVM loginVM = new LoginVM("admin", "admin", true);
+
+        repository.login(loginVM, loginPresenter);
+
+    }
+
+
+
+}
