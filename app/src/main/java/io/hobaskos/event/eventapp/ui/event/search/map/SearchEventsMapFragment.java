1c10aa0743d8389c9f8e7b8fc1da7f7e38b4744a
André Hovda <andre.hovda@gmail.com>
2017-03-13 13:27:46 +0100

    update to eventsmapfragment

--- app/src/main/java/io/hobaskos/event/eventapp/ui/event/search/map/SearchEventsMapFragment.java
+++ app/src/main/java/io/hobaskos/event/eventapp/ui/event/search/map/SearchEventsMapFragment.java
@@ -7,7 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import io.hobaskos.event.eventapp.R;
 
@@ -15,41 +21,43 @@ import io.hobaskos.event.eventapp.R;
  * Created by test on 3/13/2017.
  */
 
-public class SearchEventsMapFragment extends Fragment {
+public class SearchEventsMapFragment extends Fragment implements OnMapReadyCallback {
 
     public final static String TAG = SearchEventsMapFragment.class.getName();
 
-    protected MapView mMapView;
+    private MapView mapView;
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_events_map, parent, false);
-        mMapView = (MapView) view.findViewById(R.id.fragment_search_events_map_mapview);
-        mMapView.onCreate(savedInstanceState);
+        mapView = (MapView) view.findViewById(R.id.fragment_search_events_map_mapview);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if (mMapView != null) {
-            mMapView.onResume();
+        if (mapView != null) {
+            mapView.onResume();
         }
     }
 
     @Override
     public void onPause() {
-        if (mMapView != null) {
-            mMapView.onPause();
+        if (mapView != null) {
+            mapView.onPause();
         }
         super.onPause();
     }
 
     @Override
     public void onDestroy() {
-        if (mMapView != null) {
+        if (mapView != null) {
             try {
-                mMapView.onDestroy();
+                mapView.onDestroy();
             } catch (NullPointerException e) {
                 Log.e(TAG, "Error while attempting MapView.onDestroy(), ignoring exception", e);
             }
@@ -60,16 +68,26 @@ public class SearchEventsMapFragment extends Fragment {
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        if (mMapView != null) {
-            mMapView.onLowMemory();
+        if (mapView != null) {
+            mapView.onLowMemory();
         }
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (mMapView != null) {
-            mMapView.onSaveInstanceState(outState);
+        if (mapView != null) {
+            mapView.onSaveInstanceState(outState);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        if (googleMap != null) {
+            googleMap.getUiSettings().setZoomControlsEnabled(true);
+            googleMap.addMarker(new MarkerOptions().position(new LatLng(43.1, -87.9)));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10));
         }
     }
 }
