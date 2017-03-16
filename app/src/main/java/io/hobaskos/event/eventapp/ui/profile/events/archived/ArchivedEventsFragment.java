613ad1d5e42f101997ca3dce944e9d97b12b4e4c
André Hovda <andre.hovda@gmail.com>
2017-03-16 16:26:12 +0100

    Updates ProfilePagerAdapter

---
+++ app/src/main/java/io/hobaskos/event/eventapp/ui/profile/events/archived/ArchivedEventsFragment.java
@@ -0,0 +1,10 @@
+package io.hobaskos.event.eventapp.ui.profile.events.archived;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by test on 3/16/2017.
+ */
+
+public class ArchivedEventsFragment extends Fragment {
+}
