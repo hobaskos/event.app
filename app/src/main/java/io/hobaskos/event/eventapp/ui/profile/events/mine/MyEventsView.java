acb0061294c89ef9da8bf41fcd99c9b2160e5d01
André Hovda <andre.hovda@gmail.com>
2017-03-17 11:36:15 +0100

    #FRONTEND-99 connects presenter to repo

--- app/src/main/java/io/hobaskos/event/eventapp/ui/profile/events/mine/MyEventsView.java
+++ app/src/main/java/io/hobaskos/event/eventapp/ui/profile/events/mine/MyEventsView.java
@@ -2,10 +2,15 @@ package io.hobaskos.event.eventapp.ui.profile.events.mine;
 
 import com.hannesdorfmann.mosby.mvp.MvpView;
 
+import java.util.List;
+
+import io.hobaskos.event.eventapp.data.model.Event;
+import io.hobaskos.event.eventapp.ui.base.view.viewcontract.MvpLoadMoreView;
+
 /**
  * Created by test on 3/16/2017.
  */
 
-public interface MyEventsView extends MvpView {
+public interface MyEventsView extends MvpLoadMoreView<List<Event>> {
 
 }
