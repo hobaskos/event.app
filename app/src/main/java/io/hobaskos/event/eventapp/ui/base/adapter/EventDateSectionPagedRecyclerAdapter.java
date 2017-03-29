72483c537aa5434e7b448f455e8ccc76866cdda8
André Hovda <andre.hovda@gmail.com>
2017-03-30 01:16:44 +0200

    Adds next year section to event list

--- app/src/main/java/io/hobaskos/event/eventapp/ui/base/adapter/EventDateSectionPagedRecyclerAdapter.java
+++ app/src/main/java/io/hobaskos/event/eventapp/ui/base/adapter/EventDateSectionPagedRecyclerAdapter.java
@@ -45,6 +45,7 @@ public class EventDateSectionPagedRecyclerAdapter extends SectionedPagedRecycler
     private Section<Event> sectionOctober;
     private Section<Event> sectionNovember;
     private Section<Event> sectionDecember;
+    private Section<Event> sectionNextYear;
 
 
     public EventDateSectionPagedRecyclerAdapter(Context context, Action1<Event> onItemClick) {
@@ -70,6 +71,7 @@ public class EventDateSectionPagedRecyclerAdapter extends SectionedPagedRecycler
         sectionOctober = new Section<>(context.getResources().getString(R.string.october));
         sectionNovember = new Section<>(context.getResources().getString(R.string.november));
         sectionDecember = new Section<>(context.getResources().getString(R.string.december));
+        sectionNextYear = new Section<>(context.getResources().getString(R.string.next_year));
     }
 
     public List<Event> getItems() {
@@ -108,61 +110,65 @@ public class EventDateSectionPagedRecyclerAdapter extends SectionedPagedRecycler
         totalPositions += 1;
     }
 
-    //TODO: Factor in year
     private void addItemsToDatedSections(List<Event> items) {
         Log.d(TAG, "addItemsToDatedSections");
         DateTime currDate = DateTime.now();
         for (Event item : items) {
             DateTime date = item.getFromDate();
-            //Log.d(TAG, "getMonthOfYear: " + date.getMonthOfYear());
-            // Today:
-            if (date.dayOfYear().equals(currDate.dayOfYear())) {
-                insertItem(item, sectionToday, "Today");
-                // Tomorrow:
-            } else if ((date.dayOfYear().get() - currDate.dayOfYear().get()) == 1) {
-                insertItem(item, sectionTomorrow, "Tomorrow");
-                //This week:
-            } else if (date.weekOfWeekyear().equals(currDate.weekOfWeekyear())) {
-                insertItem(item, sectionThisWeek, "This week");
-                //Next week:
-            } else if ((date.weekOfWeekyear().get() - currDate.weekOfWeekyear().get()) == 1) {
-                insertItem(item, sectionNextWeek, "Next week");
-                // January:
-            } else if (date.getMonthOfYear() == 1) {
-                insertItem(item, sectionJanuary, "January");
-                //February:
-            } else if (date.getMonthOfYear() == 2) {
-                insertItem(item, sectionFebruary, "February");
-                //March:
-            } else if (date.getMonthOfYear() == 3) {
-                insertItem(item, sectionMarch, "March");
-                //April:
-            } else if (date.getMonthOfYear() == 4) {
-                insertItem(item, sectionApril, "April");
-                //May:
-            } else if (date.getMonthOfYear() == 5) {
-                insertItem(item, sectionMay, "May");
-                //June:
-            } else if (date.getMonthOfYear() == 6) {
-                insertItem(item, sectionJune, "June");
-                //July:
-            } else if (date.getMonthOfYear() == 7) {
-                insertItem(item, sectionJuly, "July");
-                //August:
-            } else if (date.getMonthOfYear() == 8) {
-                insertItem(item, sectionAugust, "August");
-                //September:
-            } else if (date.getMonthOfYear() == 9) {
-                insertItem(item, sectionSeptember, "September");
-                //October:
-            } else if (date.getMonthOfYear() == 10) {
-                insertItem(item, sectionOctober, "October");
-                //November:
-            } else if (date.getMonthOfYear() == 11) {
-                insertItem(item, sectionNovember, "November");
-                //December:
-            } else if (date.getMonthOfYear() == 12) {
-                insertItem(item, sectionDecember, "December");
+            // Make sure that year is current year
+            if (date.getYear() == currDate.getYear()) {
+                // Today:
+                if (date.dayOfYear().equals(currDate.dayOfYear())) {
+                    insertItem(item, sectionToday, "Today");
+                    // Tomorrow:
+                } else if ((date.dayOfYear().get() - currDate.dayOfYear().get()) == 1) {
+                    insertItem(item, sectionTomorrow, "Tomorrow");
+                    //This week:
+                } else if (date.weekOfWeekyear().equals(currDate.weekOfWeekyear())) {
+                    insertItem(item, sectionThisWeek, "This week");
+                    //Next week:
+                } else if ((date.weekOfWeekyear().get() - currDate.weekOfWeekyear().get()) == 1) {
+                    insertItem(item, sectionNextWeek, "Next week");
+                    // January:
+                } else if (date.getMonthOfYear() == 1) {
+                    insertItem(item, sectionJanuary, "January");
+                    //February:
+                } else if (date.getMonthOfYear() == 2) {
+                    insertItem(item, sectionFebruary, "February");
+                    //March:
+                } else if (date.getMonthOfYear() == 3) {
+                    insertItem(item, sectionMarch, "March");
+                    //April:
+                } else if (date.getMonthOfYear() == 4) {
+                    insertItem(item, sectionApril, "April");
+                    //May:
+                } else if (date.getMonthOfYear() == 5) {
+                    insertItem(item, sectionMay, "May");
+                    //June:
+                } else if (date.getMonthOfYear() == 6) {
+                    insertItem(item, sectionJune, "June");
+                    //July:
+                } else if (date.getMonthOfYear() == 7) {
+                    insertItem(item, sectionJuly, "July");
+                    //August:
+                } else if (date.getMonthOfYear() == 8) {
+                    insertItem(item, sectionAugust, "August");
+                    //September:
+                } else if (date.getMonthOfYear() == 9) {
+                    insertItem(item, sectionSeptember, "September");
+                    //October:
+                } else if (date.getMonthOfYear() == 10) {
+                    insertItem(item, sectionOctober, "October");
+                    //November:
+                } else if (date.getMonthOfYear() == 11) {
+                    insertItem(item, sectionNovember, "November");
+                    //December:
+                } else if (date.getMonthOfYear() == 12) {
+                    insertItem(item, sectionDecember, "December");
+                }
+            } else {
+                // Next year
+                insertItem(item, sectionNextYear, "Next year");
             }
         }
     }
@@ -222,120 +228,13 @@ public class EventDateSectionPagedRecyclerAdapter extends SectionedPagedRecycler
             attendanceCount = (TextView) itemView.findViewById(R.id.attendance_count);
             myAttendance = (TextView) itemView.findViewById(R.id.my_attendance);
             eventDate = (TextView) itemView.findViewById(R.id.event_time);
-            //categoryColor = itemView.findViewById(R.id.category_theme_color);
-            //categorySubColor = itemView.findViewById(R.id.category_theme_color_sub);
             cardView = (CardView) itemView.findViewById(R.id.card_view);
             eventImage = (ImageView) itemView.findViewById(R.id.event_list_item_image);
 
-
-            //eventId = (TextView) itemView.findViewById(R.id.event_id);
-            //background = (ImageView) itemView.findViewById(R.id.image);
         }
 
         public void click(final Event event, Action1<Event> listener) {
             itemView.setOnClickListener((i) -> listener.call(event));
         }
     }
-
-/*
-    // NOT CURRENTLY IN USE
-    private boolean insertItemIfEligible(Event event, Section section, String title, Predicate<Event> tester) {
-        if (tester.test(event)) {
-            if (section == null) {
-                section = new Section(title);
-                activeSectionsList.add(section);
-                // Update total position count to include header/section:
-                totalPositions += 1;
-            }
-            // Add item/event
-            section.addItem(event);
-            // Update total position count to include item:
-            totalPositions += 1;
-            return true;
-        }
-        return false;
-    }
-
-    // NOT CURRENTLY IN USE
-    private void addItemsToDatedSections2(List<Event> items) {
-        DateTime currDate = DateTime.now();
-        for (Event item : items) {
-            // Today:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionTomorrow, "Tomorrow", (Event e) -> Days.daysBetween(currDate, e.getFromDate()).getDays() == 1);
-            //This week::
-            insertItemIfEligible(item, sectionThisWeek, "This week", (Event e) -> e.getFromDate().weekOfWeekyear().equals(currDate.weekOfWeekyear()));
-            // TODO
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));// Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));// Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-            // Tomorrow:
-            insertItemIfEligible(item, sectionToday, "Today", (Event e) -> e.getFromDate().dayOfYear().equals(currDate.dayOfYear()));
-
-
-//            } else if (date.weekOfWeekyear().equals(currDate.weekOfWeekyear())) {
-//                sectionThisWeek.addItem(item);
-//                //Next week:
-//            } else if ((Weeks.weeksBetween(currDate, date).getWeeks() == 1)) {
-//                sectionNextWeek.addItem(item);
-//                // January:
-//            } else if (date.getMonthOfYear() == 1) {
-//                sectionJanuary.addItem(item);
-//                //February:
-//            } else if (date.getMonthOfYear() == 2) {
-//                sectionFebruary.addItem(item);
-//                //March:
-//            } else if (date.getMonthOfYear() == 3) {
-//                sectionMarch.addItem(item);
-//                //April:
-//            } else if (date.getMonthOfYear() == 4) {
-//                sectionApril.addItem(item);
-//                //May:
-//            } else if (date.getMonthOfYear() == 5) {
-//                sectionMay.addItem(item);
-//                //June:
-//            } else if (date.getMonthOfYear() == 6) {
-//                sectionJune.addItem(item);
-//                //July:
-//            } else if (date.getMonthOfYear() == 7) {
-//                sectionJuly.addItem(item);
-//                //August:
-//            } else if (date.getMonthOfYear() == 8) {
-//                sectionAugust.addItem(item);
-//                //September:
-//            } else if (date.getMonthOfYear() == 9) {
-//                sectionSeptember.addItem(item);
-//                //October:
-//            } else if (date.getMonthOfYear() == 10) {
-//                sectionOctober.addItem(item);
-//                //November:
-//            } else if (date.getMonthOfYear() == 11) {
-//                sectionNovember.addItem(item);
-//                //December:
-//            } else if (date.getMonthOfYear() == 12) {
-//                sectionDecember.addItem(item);
-//            }
-
-        }
-    }
-
-    */
 }
